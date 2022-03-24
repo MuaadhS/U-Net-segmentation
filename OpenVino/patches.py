97e889c7f95b916a0d166147345a86c16eef9511
Muaadh Shamhan <78462850+MuaadhS@users.noreply.github.com>
2022-03-24 22:04:11 +0800

    Add files via upload

--- OpenVino/patches.py
+++ OpenVino/patches.py
@@ -1,11 +1,11 @@
-
+# -*- coding: utf-8 -*-
 """
-Created on Mon Mar 21 11:37:14 2022
+Created on Tue Mar 22 23:35:25 2022
 
-@author: muaadh
+@author: moaat
 """
+
 import time
-
 start = time.time()
 
 import cv2
@@ -14,35 +14,6 @@ import numpy as np
 from openvino.inference_engine import IECore
 from patchify import patchify, unpatchify
 
-####
-from argparse import ArgumentParser, SUPPRESS
-from pathlib import Path
-import logging
-
-logging.basicConfig(format='[ %(levelname)s ] %(message)s', level=logging.INFO)
-log = logging.getLogger()
-
-def build_argparser():    
-    parser = ArgumentParser(add_help=False)
-    args = parser.add_argument_group('Options')
-
-    args.add_argument('-h', '--help', action='help', default=SUPPRESS, help='Show this help message and exit.')
-
-    args.add_argument('-m', '--model', help='Required. Path to an .xml file with a trained model.',
-                          required=True, type=Path)
-
-    
-    args.add_argument('-i', '--input', required=True,
-                          help='Required. An input to process. The input must an image.')
-
-    args.add_argument('-d', '--device', default='CPU', type=str,
-                          help='Optional. Specify the target device to infer on; CPU, GPU, HDDL or MYRIAD is '
-                               'acceptable. The demo will look for a suitable plugin for device specified. '
-                               'Default value is CPU.')
-    
-    return parser
-
-####
 
 def segmentation_map_to_image(
     result: np.ndarray, colormap: np.ndarray, remove_holes=False
@@ -50,7 +21,6 @@ def segmentation_map_to_image(
     """
     Convert network result of floating point numbers to an RGB image with
     integer values from 0-255 by applying a colormap.
-
     :param result: A single network result after converting to pixel values in H,W or 1,H,W shape.
     :param colormap: A numpy array of shape (num_classes, 3) with an RGB value per class.
     :param remove_holes: If True, remove holes in the segmentation result.
@@ -103,64 +73,24 @@ def normalize(x, axis=-1, order=2):
     l2[l2 == 0] = 1
     return x / np.expand_dims(l2, axis)
 
-####
-args = build_argparser().parse_args()
-####
 
 # Initialize inference engine core and read the IR model 
-log.info('Initializing Inference Engine...')
 ie = IECore()
 
 net = ie.read_network(
-    model=args.model)
-    #model="model/saved_model.xml")
+    model="model/saved_model.xml")
     
-exec_net = ie.load_network(net, args.device)
-#exec_net = ie.load_network(net, "CPU")
+exec_net = ie.load_network(net, "CPU")
 
 output_layer_ir = next(iter(exec_net.outputs))
 input_layer_ir = next(iter(exec_net.input_info))
 
 
 
-'''
+
 # Configure the input 
-#image = cv2.imread(args["input"], cv2.IMREAD_GRAYSCALE)
-image = cv2.imread(args.input, 0)
-#image = cv2.imread(r"image__3_00.tif", 0)
-#image = cv2.imread(r"d1-c1-002.jpg", 0)
-
-
-N, C, H, W = net.input_info[input_layer_ir].tensor_desc.dims
-
-#resized_image = cv2.resize(image, (512, 512))
-image = cv2.resize(image, (512, 512))
-image_h, image_w, = image.shape
-
-image_norm = np.expand_dims(normalize(np.array(image), axis=1),2)
-image_norm = image_norm[:,:,0][:,:,None]
-input_image = np.expand_dims(image_norm.transpose(2, 0, 1), 0)
-
-
-rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-plt.imshow(image, cmap = 'gray')
-
-
-
-# Run the infernece
-result = exec_net.infer(inputs={input_layer_ir: input_image})
-result_ir = result[output_layer_ir]
- 
-# Prepare data for visualization
-segmentation_mask=(result_ir[0,0,:,:] > 0.5).astype(np.uint8)
-
-
-plt.imshow(segmentation_mask, cmap='viridis')
-'''
-# Configure the input 
-image = cv2.imread(args.input, 0)
-#image = cv2.imread(r"d1-c1-002.jpg", 0)
+image = cv2.imread(r"CLAHE_Image_screenshot_24.03.2022.png", 0)
+#image = cv2.imread(r"image__1_03.png", 0)
 N, C, H, W = net.input_info[input_layer_ir].tensor_desc.dims
 
 image_norm = np.expand_dims(normalize(np.array(image), axis=1),2)
@@ -174,7 +104,7 @@ image_h, image_w, = image.shape
 predicted_patches = []
 for i in range(patches.shape[0]):
     for j in range(patches.shape[1]):
-#        print(i,j)
+        print(i,j)
         
         #single_patch = patches[i,j,:,:]
         #single_patch_norm = np.expand_dims(normalize(np.array(single_patch), axis=1),2)
@@ -208,6 +138,32 @@ segmentation_mask = unpatchify(predicted_patches_reshaped, image.shape)
 rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 
+'''
+#resized_image = cv2.resize(image, (512, 512))
+image = cv2.resize(image, (512, 512))
+image_h, image_w, = image.shape
+
+image_norm = np.expand_dims(normalize(np.array(image), axis=1),2)
+image_norm = image_norm[:,:,0][:,:,None]
+input_image = np.expand_dims(image_norm.transpose(2, 0, 1), 0)
+
+
+rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+plt.imshow(image, cmap = 'gray')
+
+
+
+# Run the infernece
+result = exec_net.infer(inputs={input_layer_ir: input_image})
+result_ir = result[output_layer_ir]
+ 
+# Prepare data for visualization
+segmentation_mask=(result_ir[0,0,:,:] > 0.5).astype(np.uint8)
+'''
+
+#plt.imshow(segmentation_mask, cmap='viridis')
+
 
 
 # Define colormap, each color represents a class
@@ -226,19 +182,18 @@ resized_mask = cv2.resize(mask, (image_w, image_h))
 image_with_mask = cv2.addWeighted(resized_mask, alpha, rgb_image, 1 - alpha, 0)
 
 numberPixels = len(cv2.findNonZero(segmentation_mask))
-#print(numberPixels)
+print(numberPixels)
 
 img_area = segmentation_mask.shape[0] * segmentation_mask.shape[1]
-segmented_cells = (1-((img_area - numberPixels)/ img_area)) * 100
+segmented_cells = ((img_area - numberPixels)/ img_area) * 100
+
+#print("The predected confluency is: ", segmented_cells, "%")
+conf=("The predected confluency is: ", segmented_cells, "%")
+print(conf)
+#Show segmentation output
 
 
-conf=("The estimated confluency is (%): ")
 
-print(conf + str(segmented_cells))
-#log.info(conf)
-
-
-#Show segmentation output 
 plt.figure(figsize=(15, 10))
 plt.subplot(231)
 plt.title('Image')
@@ -249,33 +204,7 @@ plt.imshow(segmentation_mask, cmap='viridis')
 plt.subplot(233)
 plt.title('Image with mask')
 plt.imshow(image_with_mask)
+#plt.hist(segmentation_mask.flatten())
 
 end = time.time()
 print(end - start)
-
-plt.show()
-#plt.savefig('myfilename.png', dpi=100)
-
-'''
-result_directory = args.
-plt.tight_layout()
-img_name_name = os.path.join(results_directory, "pred{}.png".format(img_no))
-plt.savefig(png_name, bbox_inches="tight", pad_inches=0)
-'''
-
-##############################################################
-
-'''
-#IOU
-y_pred = result_ir
-y_pred_thresholded = y_pred > 0.5
-
-intersection = np.logical_and(segmentation_mask, y_pred_thresholded)
-union = np.logical_or(segmentation_mask, y_pred_thresholded)
-iou_score = np.sum(intersection) / np.sum(union)
-print("IoU socre is: ", iou_score)
-'''
-
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
